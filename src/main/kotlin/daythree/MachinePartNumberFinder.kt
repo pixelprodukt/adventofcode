c1d90c871050659c55b7e9af7a3db30551e66ab3
pixelprodukt <adriansemmler@gmx.de>
2024-08-20 19:54:42 +0200

    Solution day three part one

--- src/main/kotlin/daythree/MachinePartNumberFinder.kt
+++ src/main/kotlin/daythree/MachinePartNumberFinder.kt
@@ -8,104 +8,59 @@ class MachinePartNumberFinder(
     private val inputLines = getLinesFromTextfile(fileName)
 
     init {
-        val charMatrix = mutableListOf<List<Char>>()
+        val charMatrix = mutableListOf<MutableList<Char>>()
         inputLines.forEach { line ->
             println(line.map { it })
-            charMatrix.add(line.map { it })
+            charMatrix.add(line.map { it }.toMutableList())
         }
-        //val charMap: List<List<Char>> = inputLines.map { line -> line.map { it } }
-        //println(charMap.joinToString("\n"))
 
         val machinePartNumbersToAdd = mutableListOf<Int>()
-
         var yPos = 0
         var xPos = 0
 
         while (yPos < charMatrix.size) {
-
             val lastFoundNumberCache = mutableListOf<Char>()
             val currentLine = charMatrix[yPos]
+            val symbolsFound = mutableListOf<Boolean>()
             xPos = 0 // reset x here for next line
 
             while (xPos < currentLine.size) {
 
                 val character = currentLine[xPos]
 
-                //println("$character is special character: ${isSpecialCharacter(character)}")
-
                 if (isPartOfMachineNumber(character)) {
-                    //println("isPartOfMachineNumber: $character")
 
                     lastFoundNumberCache.add(character)
 
-                    var symbolInNeighboursFound = false
+                    // scanNeighbours
 
-                    // scanNeighbours()
-                    // blabla refactor everything to one function
+                    // top line
                     if (yPos - 1 > 0) {
                         val topLine = charMatrix[yPos - 1]
-                        val topLeftChar = if(xPos - 1 < 0) null else topLine[xPos - 1]
-                        val topMiddleChar = topLine[xPos]
-                        val topRightChar = if(xPos + 1 < topLine.size) topLine[xPos + 1] else null
-
-                        println("symbolInNeighboursFound in top:     ${listOf(topLeftChar, topMiddleChar, topRightChar)}")
-                        if (
-                            listOf(
-                                isSpecialCharacter(topLeftChar),
-                                isSpecialCharacter(topMiddleChar),
-                                isSpecialCharacter(topRightChar)
-                            )
-                                .contains(true)
-                        ) {
-                            symbolInNeighboursFound = true
-                        }
+                        symbolsFound.add(lineHasSymbol(topLine, xPos))
                     }
 
-                    val currentLeftChar = if(xPos - 1 < 0) null else currentLine[xPos - 1]
-                    val currentChar = currentLine[xPos]
-                    val currentRightChar = if(xPos + 1 < currentLine.size) currentLine[xPos + 1] else null
+                    // current line
+                    symbolsFound.add(lineHasSymbol(currentLine, xPos))
 
-                    println("symbolInNeighboursFound in current: ${listOf(currentLeftChar, currentChar, currentRightChar)}")
-                    if (listOf(
-                            isSpecialCharacter(currentLeftChar),
-                            isSpecialCharacter(currentChar),
-                            isSpecialCharacter(currentRightChar)
-                        ).contains(true)
-                    ) {
-                        symbolInNeighboursFound = true
-                    }
-
+                    // bottom line
                     if (yPos + 1 < charMatrix.size) {
                         val bottomLine = charMatrix[yPos + 1]
-                        val bottomLeftChar = if(xPos - 1 < 0) null else bottomLine[xPos - 1]
-                        val bottomMiddleChar = bottomLine[xPos]
-                        val bottomRightChar = if(xPos + 1 < bottomLine.size) bottomLine[xPos + 1] else null
-
-                        println("symbolInNeighboursFound in bottom:  ${listOf(bottomLeftChar, bottomMiddleChar, bottomRightChar)}")
-                        if (listOf(
-                                isSpecialCharacter(bottomLeftChar),
-                                isSpecialCharacter(bottomMiddleChar),
-                                isSpecialCharacter(bottomRightChar)
-                            ).contains(true)
-                        ) {
-                            symbolInNeighboursFound = true
-                        }
-                    }
-
-                    // here lies the problem I think
-                    if (!isPartOfMachineNumber(currentLine[xPos + 1]) && symbolInNeighboursFound) {
-                        lastFoundNumberCache.clear()
+                        symbolsFound.add(lineHasSymbol(bottomLine, xPos))
                     }
 
                     // look ahead for end of number and compile if there is no further digit
-                    if (!isPartOfMachineNumber(currentLine[xPos + 1]) && !symbolInNeighboursFound) {
+                    if (!isPartOfMachineNumber(currentLine.getOrNull(xPos + 1)) && symbolsFound.contains(true)) {
                         println("lastNumberFoundCache: $lastFoundNumberCache")
                         // compile complete number and add to total
                         machinePartNumbersToAdd.add(lastFoundNumberCache.joinToString("").toInt())
                         // clear cache
                         lastFoundNumberCache.clear()
+                        symbolsFound.clear()
+                    } else if (!isPartOfMachineNumber(currentLine.getOrNull(xPos + 1)) && !symbolsFound.contains(true)) {
+                        lastFoundNumberCache.clear()
+                        symbolsFound.clear()
                     }
-
                 }
 
                 xPos++
@@ -115,15 +70,29 @@ class MachinePartNumberFinder(
         }
 
         println("machine part numbers: ${machinePartNumbersToAdd}")
-        println("sum of all numbers: ${machinePartNumbersToAdd.sum()}")
+        println("sum of all numbers: ${machinePartNumbersToAdd.sum()}") // should return 520019
     }
 
-    private fun isPartOfMachineNumber(character: Char): Boolean {
-        return character.digitToIntOrNull() != null
+    private fun isPartOfMachineNumber(character: Char?): Boolean {
+        return character?.digitToIntOrNull() != null
     }
 
     private fun isSpecialCharacter(character: Char?): Boolean {
-        // better use regex, doesn't seem to work on hastag "#"
-        return if (character != null) character.toString() != "#" && character.toString() != "." && character.digitToIntOrNull() == null else false
+        val pattern = "[!@#$%&*()/_+=|<>?{}\\[\\]~-]"
+        return Regex(pattern).containsMatchIn(character.toString())
+    }
+
+    private fun lineHasSymbol(line: List<Char>, xPos: Int): Boolean {
+        val leftChar = if(xPos - 1 < 0) ".".toCharArray().first() else line[xPos - 1]
+        val middleChar = line[xPos]
+        val rightChar = if(xPos + 1 < line.size) line[xPos + 1] else ".".toCharArray().first()
+
+        println("symbolInNeighboursFound in bottom:  ${listOf(leftChar, middleChar, rightChar)}")
+
+        return listOf(
+            isSpecialCharacter(leftChar),
+            isSpecialCharacter(middleChar),
+            isSpecialCharacter(rightChar)
+        ).contains(true)
     }
 }
